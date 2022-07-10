bcc133de5ec203f488c2318ab66465c535a3b4cd
SamaraBilby <samarabilby@hotmail.com>
2022-07-09 20:10:43 -0400

    models

---
+++ models/Job.js
@@ -0,0 +1,26 @@
+const Sequelize = require('sequelize');
+
+const db = require('../db/connection');
+
+const Job = db.define('job',{
+    title:{
+        type: Sequelize.STRING,
+    },
+    descricao:{
+        type: Sequelize.STRING,
+    },
+    salario: {
+        type: Sequelize.STRING,
+    },
+    empresa: {
+        type: Sequelize.STRING
+    },
+    email: {
+        type: Sequelize.STRING,
+    },
+    novo: {
+        type: Sequelize.INTEGER,
+    }    
+});
+
+module.exports = Job
